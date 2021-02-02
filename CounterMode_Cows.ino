101cb92d007c7aa4f6edf408fe6d05c76f68b2dd
Finn W <35266822+JediKnight122@users.noreply.github.com>
2021-02-02 14:08:26 +0100

    Add files via upload

---
+++ CounterMode_Cows.ino
@@ -0,0 +1,47 @@
+int button;
+
+void setup() {
+
+  pinMode(25, OUTPUT); //1= Eingang gesperrt 0=Eingang offen
+  pinMode(21, INPUT); //Taster zum Zurücksetzen. Muss an 21 oder 20 sein. 21 -> INT0_vect
+
+  cli();
+  TCNT5 = 0;
+
+  TCCR5A = 0;
+  TCCR5B = 7;
+
+  TIMSK5 = 1;
+
+  EICRA = 10;
+  EIMSK = 3;
+
+  EICRA = 10;
+  sei();
+}
+
+void loop() {
+
+
+
+  if (TCNT5 >= 10) {
+    //TCNT5 ist der externe Zaehler, der selbsständig hochzhaehlt.
+    
+    EIMSK = 3;
+    digitalWrite(25, LOW);
+
+  }
+  else {
+    
+    EIMSK = 0;
+    digitalWrite(25, HIGH);
+  }
+}
+
+
+ISR(INT0_vect) {
+ 
+    TCNT5 = 0;
+  
+}
+
---
+++ TrainManagement.ino
@@ -0,0 +1,58 @@
+int p10 = 20;
+int gleisnummer;
+int weichensignal;
+int anzeige_tab[] = {0, 1};
+int gleis_tab[] = {0, 1};
+
+void setup() {
+  init();
+
+
+}
+
+void loop() {
+  if (p10 == 0) {
+    automatik();
+
+  }
+  else if (p10 == 1) {
+    sensor_enable();
+  }
+
+}
+
+void init() {
+
+
+  DDRA = 0xff; //PORT A als Output für die Siebensegment Anzeige
+  DDRL = 0xff; //PORT L als Output für die Weichensignale
+
+  cli();
+  EICRA = 15;
+  EIMSK = 3;
+  sei();
+}
+
+void ausgabe() {
+  PORTA = anzeige_tab[gleisnummer];
+  PORTL = gleis_tab[weichensignal];
+}
+
+void sensor_enable() {
+  EIMSK = 3;
+}
+
+void automatik() {
+  EIMSK = 0;
+  zeit_5sec();
+  gleisnummer++;
+}
+
+ISR(INT0_vect) {
+  gleisnummer = 1;
+}
+
+void zeit_5sec() {
+
+}
+
