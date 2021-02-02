f3f49046ff474213fcb591feace36e89c9917651
Finn W <35266822+JediKnight122@users.noreply.github.com>
2021-02-02 14:07:41 +0100

    Add files via upload

---
+++ Guitar_Tuner.ino
@@ -0,0 +1,68 @@
+int einlesen = 0;
+boolean entscheidung = true;
+
+void setup() {
+  pinMode (6, OUTPUT);
+  TCCR3A = 0;
+  TCCR3B = 0;
+  OCR3A = 65000;
+  TCCR3B = 12;
+  TIMKS3 = 2;
+  pinMode (PORTA, INPUT);
+
+
+
+}
+
+void loop() {
+  einlesen = digitalRead(PORTA);
+
+
+  // E
+  if (einlesen >= 1 ) {
+    OCR3A = 24390;
+    TCCR3B = 10;
+  }
+
+  //A
+  else if (einlesen >= 2) {
+    OCR3A = 18181;
+    TCCR3B = 10;
+  }
+  //d
+  else if (einlesen >= 3) {
+
+    OCR3A = 13698;
+    TCCR3B = 10;
+
+  }
+  //g
+  else if (einlesen >= 4) {
+    OCR3A = 10204;
+    TCCR3B = 10;
+  }
+
+  //h
+  else if (einlesen >= 5) {
+    OCR3A = 65040;
+    TCCR3B = 9;
+  }
+
+  //e'
+  else if (einlesen >= 6) {
+    OCR3A = 48632;
+    TCCR3B = 9;
+  }
+}
+
+ISR (TIMER3_COMPA_vect) {
+  if (entscheidung == true) {
+    digitalWrite(2, HIGH);
+    entscheidung = false;
+  }
+  else {
+    digitalWrite(2, LOW);
+    entscheidung = true;
+  }
+}
+
---
+++ Race_Starting_Lights.ino
@@ -0,0 +1,59 @@
+int counter=0;
+int taster=0;
+int rennzeit=300;
+boolean umschalten=true;
+
+void setup() {
+  pinMode (PORTA, OUTPUT);
+  pinMode (6, INPUT);
+cli();
+TCCR3A=0;
+TCCR3B=0;
+OCR3A=62500;
+TCCR3A=2;
+TCCR3B=2;
+sei();
+
+
+}
+
+void loop() {
+  
+digitalWrite(PORTA, ampelphasen[zustand]);
+
+}
+
+void anzeige(){
+  
+}
+
+void ausgabeAmpel(){
+int ampelphasen[9];
+ampelphasen[0]=21;
+ampelphasen[1]=10;
+ampelphasen[2]=16;
+ampelphasen[3]=24;
+ampelphasen[4]=28;
+ampelphasen[5]=30;
+ampelphasen[6]=31;
+ampelphasen[7]=0;
+ampelphasen[8]=0; //unfertig
+
+}
+
+ISR (TIMER3_COMPA_vect){
+schalter =  digitalRead(6);
+  if (schalter==0 && zustand==0){
+   zustand=1;
+   umschalten=true;
+  }
+ if (umschalten=true){
+  zustand++;  
+ }
+
+ if (zustand==8){
+  rennzeit--;
+  anzeige();
+ }
+}
+
